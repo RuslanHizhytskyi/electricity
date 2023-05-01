0fc93eefb2e6a596bccae1bad24e3e76e41eeaa8
Ruslan Hizhytskyi <r.hizhytskyi@gmail.com>
2023-05-01 20:10:38 +0300

    Solution by Ruslan Hizhytskyi

--- src/index.js
+++ src/index.js
@@ -5,37 +5,187 @@
 
 
 export class World {
-  constructor() {}
+  constructor() {
+    this.powerPlants = new Set();
+    this.households = new Set();
+  }
 
   createPowerPlant() {
-    throw new Error("Not Implemented");
+    const newPowerPlan = {
+      alive: true,
+      id: this.uniqueId(),
+      connections: new Set(),
+    };
+    this.powerPlants.add(newPowerPlan);
+    return newPowerPlan;
   }
 
   createHousehold() {
-    throw new Error("Not Implemented");
+    const household = {
+      id: this.uniqueId(),
+      connectionsPowerPlan: new Set(),
+      connectionsHousehold: new Set(),
+      connectionsWithElectricity: new Set(),
+    };
+    this.households.add(household);
+    return household;
   }
 
   connectHouseholdToPowerPlant(household, powerPlant) {
-    throw new Error("Not Implemented");
+    if (!this.households.has(household) || !this.powerPlants.has(powerPlant)) {
+      throw new Error("Invalid Household or PowerPlant");
+    }
+
+    household.connectionsPowerPlan.add(powerPlant.id);
+    powerPlant.connections.add(household.id);
+    if (powerPlant.alive) {
+      if (!this.householdHasElectricity(household)) {
+        household.connectionsWithElectricity.add(powerPlant.id);
+        this.householdUpdateElectricity(household, true);
+      } else {
+        household.connectionsWithElectricity.add(powerPlant.id);
+      }
+    }
   }
 
   connectHouseholdToHousehold(household1, household2) {
-    throw new Error("Not Implemented");
+    if (!this.households.has(household1) || !this.households.has(household2)) {
+      throw new Error("Invalid Household");
+    }
+
+    household1.connectionsHousehold.add(household2.id);
+    household2.connectionsHousehold.add(household1.id);
+
+    if (household1.connectionsWithElectricity.size > 0 || household2.connectionsWithElectricity.size > 0) {
+      if (household1.connectionsWithElectricity.size === 0) {
+        household1.connectionsWithElectricity.add(household2.id);
+        household2.connectionsWithElectricity.add(household1.id);
+        this.householdUpdateElectricity(household1, true);
+      }
+      if (household2.connectionsWithElectricity.size === 0) {
+        household1.connectionsWithElectricity.add(household2.id);
+        household2.connectionsWithElectricity.add(household1.id);
+        this.householdUpdateElectricity(household2, true);
+      }
+    }
   }
 
   disconnectHouseholdFromPowerPlant(household, powerPlant) {
-    throw new Error("Not Implemented");
+    if (!this.households.has(household) || !this.powerPlants.has(powerPlant)) {
+      throw new Error("Invalid Household or PowerPlant");
+    }
+    household.connectionsPowerPlan.delete(powerPlant.id)
+    household.connectionsWithElectricity.delete(powerPlant.id)
+    powerPlant.connections.delete(household.id)
+    if (household.connectionsWithElectricity.size === 0) {
+      this.householdUpdateElectricity(household, false);
+    }
   }
 
-  killPowerPlant(powerPlant) {
-    throw new Error("Not Implemented");
+  householdUpdateElectricity(household, isRepair = false) {
+    if (isRepair) {
+      for (const value of household.connectionsHousehold) household.connectionsWithElectricity.add(value)
+    }  else {
+      household.connectionsWithElectricity = new Set();
+    }
+    const checkedHouseholdIdList = new Set([household.id]);
+    let householdListForCheck = new Set([...household.connectionsHousehold]);
+
+    while (!this.hasAllValues(householdListForCheck, checkedHouseholdIdList)) {
+      for (const householdId of householdListForCheck.values()) {
+        const householdItem = this.findHouseholdById(householdId);
+        if (!checkedHouseholdIdList.has(householdId)) {
+          if (isRepair) {
+            for (const value of householdItem.connectionsHousehold) householdItem.connectionsWithElectricity.add(value);
+          } else {
+            householdItem.connectionsWithElectricity = new Set()
+          }
+
+          checkedHouseholdIdList.add(householdId);
+          householdListForCheck.delete(householdId)
+          householdListForCheck.add(...householdItem.connectionsHousehold);
+        }
+      }
+    }
   }
 
-  repairPowerPlant(powerPlant) {
-    throw new Error("Not Implemented");
+  findHouseholdById(id) {
+    for (const household of this.households.values()) {
+      if (household.id === id) return household
+    }
   }
 
-  householdHasEletricity(household) {
-    throw new Error("Not Implemented");
+  hasAllValues(set, inSet) {
+    for (let value of set) {
+      if (!inSet.has(value)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  killPowerPlant(powerPlan) {
+    if (!this.powerPlants.has(powerPlan)) {
+      throw new Error("Invalid PowerPlant");
+    }
+
+    if (!powerPlan.alive) {
+      return;
+    }
+
+    powerPlan.alive = false;
+
+    if ([...this.powerPlants].every(powerPlan => powerPlan.alive === false)) {
+      for (const household of this.households.values()) {
+        household.connectionsWithElectricity = new Set();
+      }
+      return;
+    }
+
+    for (const householdId of powerPlan.connections.values()) {
+      let household = this.findHouseholdById(householdId);
+      if (household) {
+        household.connectionsWithElectricity.delete(powerPlan.id)
+
+        if (household.connectionsWithElectricity.size === 0 && household.connectionsHousehold.size > 0) {
+          this.householdUpdateElectricity(household, false);
+        }
+      }
+    }
+  }
+
+  repairPowerPlant(powerPlan) {
+    if (!this.powerPlants.has(powerPlan)) {
+      throw new Error("Invalid PowerPlant");
+    }
+
+    if (powerPlan.alive) {
+      return;
+    }
+
+    powerPlan.alive = true;
+    for (const householdId of powerPlan.connections.values()) {
+      let household = this.findHouseholdById(householdId);
+      if (household) {
+        if (!this.householdHasElectricity(household)) {
+          this.householdUpdateElectricity(household, true);
+        }
+        household.connectionsWithElectricity.add(powerPlan.id)
+      }
+    }
+  }
+
+  householdHasElectricity(household) {
+    if (!this.households.has(household)) {
+      throw new Error("Invalid Household");
+    }
+
+    return household.connectionsWithElectricity.size > 0;
+  }
+
+  uniqueId() {
+    const dateString = Date.now().toString(36);
+    const randomness = Math.random().toString(36).substr(2);
+    return dateString + randomness;
   }
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -15,7 +15,7 @@ describe("Households + Power Plants", function () {
     it("Household has no electricity by default", () => {
         const world = new World();
         const household = world.createHousehold();
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
     });
 
     it("Household has electricity if connected to a Power Plant", () => {
@@ -25,7 +25,7 @@ describe("Households + Power Plants", function () {
 
         world.connectHouseholdToPowerPlant(household, powerPlant);
 
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
     });
 
     it("Household won't have Electricity after disconnecting from the only Power Plant", () => {
@@ -35,10 +35,10 @@ describe("Households + Power Plants", function () {
 
         world.connectHouseholdToPowerPlant(household, powerPlant);
 
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
 
         world.disconnectHouseholdFromPowerPlant(household, powerPlant);
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
     });
 
     it("Household will have Electricity as long as there's at least 1 alive Power Plant connected", () => {
@@ -53,16 +53,16 @@ describe("Households + Power Plants", function () {
         world.connectHouseholdToPowerPlant(household, powerPlant2);
         world.connectHouseholdToPowerPlant(household, powerPlant3);
 
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
 
         world.disconnectHouseholdFromPowerPlant(household, powerPlant1);
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
 
         world.killPowerPlant(powerPlant2);
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
 
         world.disconnectHouseholdFromPowerPlant(household, powerPlant3);
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
     });
 
     it("Household won't have Electricity if the only Power Plant dies", () => {
@@ -72,10 +72,10 @@ describe("Households + Power Plants", function () {
 
         world.connectHouseholdToPowerPlant(household, powerPlant);
 
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
 
         world.killPowerPlant(powerPlant);
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
     });
 
     it("PowerPlant can be repaired", () => {
@@ -85,19 +85,19 @@ describe("Households + Power Plants", function () {
 
         world.connectHouseholdToPowerPlant(household, powerPlant);
 
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
 
         world.killPowerPlant(powerPlant);
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
 
         world.repairPowerPlant(powerPlant);
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
 
         world.killPowerPlant(powerPlant);
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
 
         world.repairPowerPlant(powerPlant);
-        assert.equal(world.householdHasEletricity(household), true);
+        assert.equal(world.householdHasElectricity(household), true);
     });
 
     it("Few Households + few Power Plants, case 1", () => {
@@ -113,16 +113,16 @@ describe("Households + Power Plants", function () {
         world.connectHouseholdToPowerPlant(household1, powerPlant2);
         world.connectHouseholdToPowerPlant(household2, powerPlant2);
 
-        assert.equal(world.householdHasEletricity(household1), true);
-        assert.equal(world.householdHasEletricity(household2), true);
+        assert.equal(world.householdHasElectricity(household1), true);
+        assert.equal(world.householdHasElectricity(household2), true);
 
         world.killPowerPlant(powerPlant2);
-        assert.equal(world.householdHasEletricity(household1), true);
-        assert.equal(world.householdHasEletricity(household2), false);
+        assert.equal(world.householdHasElectricity(household1), true);
+        assert.equal(world.householdHasElectricity(household2), false);
 
         world.killPowerPlant(powerPlant1);
-        assert.equal(world.householdHasEletricity(household1), false);
-        assert.equal(world.householdHasEletricity(household2), false);
+        assert.equal(world.householdHasElectricity(household1), false);
+        assert.equal(world.householdHasElectricity(household2), false);
     });
 
     it("Few Households + few Power Plants, case 2", () => {
@@ -140,16 +140,16 @@ describe("Households + Power Plants", function () {
 
         world.disconnectHouseholdFromPowerPlant(household2, powerPlant2);
 
-        assert.equal(world.householdHasEletricity(household1), true);
-        assert.equal(world.householdHasEletricity(household2), false);
+        assert.equal(world.householdHasElectricity(household1), true);
+        assert.equal(world.householdHasElectricity(household2), false);
 
         world.killPowerPlant(powerPlant2);
-        assert.equal(world.householdHasEletricity(household1), true);
-        assert.equal(world.householdHasEletricity(household2), false);
+        assert.equal(world.householdHasElectricity(household1), true);
+        assert.equal(world.householdHasElectricity(household2), false);
 
         world.killPowerPlant(powerPlant1);
-        assert.equal(world.householdHasEletricity(household1), false);
-        assert.equal(world.householdHasEletricity(household2), false);
+        assert.equal(world.householdHasElectricity(household1), false);
+        assert.equal(world.householdHasElectricity(household2), false);
     });
 
     it("Household + Power Plant, case 1", () => {
@@ -158,12 +158,12 @@ describe("Households + Power Plants", function () {
         const household = world.createHousehold();
         const powerPlant = world.createPowerPlant();
 
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
         world.killPowerPlant(powerPlant);
 
         world.connectHouseholdToPowerPlant(household, powerPlant);
 
-        assert.equal(world.householdHasEletricity(household), false);
+        assert.equal(world.householdHasElectricity(household), false);
     });
 
     describe('complex network', () => {
@@ -192,18 +192,62 @@ describe("Households + Power Plants", function () {
             world.connectHouseholdToHousehold(households[getRandomInt()], households[getRandomInt()])
 
 
-            assert.equal(world.householdHasEletricity(households[getRandomInt()]), true);
-            assert.equal(world.householdHasEletricity(households[getRandomInt()]), true);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
 
             world.killPowerPlant(powerPlant1);
 
-            assert.equal(world.householdHasEletricity(households[getRandomInt()]), true);
-            assert.equal(world.householdHasEletricity(households[getRandomInt()]), true);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
 
             world.killPowerPlant(powerPlant2);
 
-            assert.equal(world.householdHasEletricity(households[getRandomInt()]), false);
-            assert.equal(world.householdHasEletricity(households[getRandomInt()]), false);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), false);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), false);
+        });
+    });
+    describe('complex network my own', () => {
+        forEach([
+            [30],
+            [100],
+        ]).it('Complex network %d', (size) => {
+            const world = new World();
+            const households = (new Array(size)).fill(undefined).map(() => world.createHousehold());
+
+            const getRandomInt = (max = size) => {
+                return Math.floor(Math.random() * max);
+            }
+
+            const powerPlant1 = world.createPowerPlant();
+            const powerPlant2 = world.createPowerPlant();
+
+            households.forEach((household, index) => {
+                world.connectHouseholdToHousehold(household, households[index === households.length - 1 ? 0 : index + 1])
+            });
+
+            world.connectHouseholdToHousehold(households[getRandomInt()], households[getRandomInt()])
+            world.connectHouseholdToHousehold(households[getRandomInt()], households[getRandomInt()])
+
+            world.connectHouseholdToPowerPlant(households[getRandomInt()], powerPlant1);
+            world.connectHouseholdToPowerPlant(households[getRandomInt()], powerPlant2);
+
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
+
+            world.killPowerPlant(powerPlant1);
+
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
+
+            world.killPowerPlant(powerPlant2);
+
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), false);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), false);
+
+            world.repairPowerPlant(powerPlant2);
+
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
+            assert.equal(world.householdHasElectricity(households[getRandomInt()]), true);
         });
     });
 });
